091f999685357c4bf64d8bba86b07166c34e6e63
Diego Fernandes <d.falves@alumnos.upm.es>
2023-05-15 18:32:58 +0200

    .

--- Main/Funciones.c
+++ Main/Funciones.c
@@ -2,12 +2,38 @@
 
 typedef struct
 {
-    char nombre1[1000];
-    char nombre2[1000];
-    char separador[1000];
-    char fecha[1000];
+    int day;
+    int month;
+    int year;
 
-}cabecera;
+}FECHA;
+
+
+
+typedef struct
+{
+
+    FECHA fechas[23];
+    float hidraulica[23];
+    float turbinacion[23];
+    float nuclear[23];
+    float carbon[23];
+    float fuel_gas[23];
+    float mot_diesel[23];
+    float turbina_gas[23];
+    float turbina_vapor[23];
+    float ciclo_combinado[23];
+    float hidroeolica[23];
+    float eolica[23];
+    float solar_fotovoltaica[23];
+    float solar_termica[23];
+    float otras_renovables[23];
+    float cogeneracion[23];
+    float residuos_no_renovables[23];
+    float residuos_renovables[23];
+    float generacion_total[23];
+
+}DATOS_GEN;
 
 int elegir_fichero(FILE *P)
 {
@@ -31,7 +57,7 @@ int elegir_fichero(FILE *P)
 
     if(fichero==1)
     {
-        P = fopen("generacion2122.csv","r");
+        P = fopen("generacion2122.txt","r");
         if (P == NULL)
             {
                 printf("Error al abrir el fichero.\n");
@@ -88,7 +114,8 @@ int elegir_fichero(FILE *P)
 void estadistica(void)
 {
     FILE *P;
-    int opcion = elegir_fichero(P);
+    int opcion = elegir_fichero(P),i=0;
+    DATOS_GEN datos;
 
     if(opcion==-1)
     {
@@ -96,9 +123,27 @@ void estadistica(void)
     }
     if(opcion==1)
     {
-        printf("TAL.\n");
+        printf("TAL.\n\n");
 
-        P =
+
+        while(fscanf(P,"%i/%i/%i %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f",
+                     &datos.fechas[i].day,&datos.fechas[i].month,&datos.fechas[i].year,
+                     &datos.hidraulica[i],&datos.turbinacion[i],&datos.nuclear[i],
+                     &datos.carbon[i],&datos.fuel_gas[i],&datos.mot_diesel[i],
+                     &datos.turbina_gas[i],&datos.turbina_vapor[i],&datos.ciclo_combinado[i],
+                     &datos.hidroeolica[i],&datos.eolica[i],&datos.solar_fotovoltaica[i],
+                     &datos.solar_termica[i],&datos.otras_renovables[i],&datos.cogeneracion[i],
+                     &datos.residuos_no_renovables[i],&datos.residuos_renovables[i],&datos.generacion_total[i])!=EOF)
+        {
+            i++;
+        }
+
+        for(i=0;i<=100;i++)
+        {
+            printf(".");
+            printf("%c\t",datos.carbon[i]);
+        }
+
     }
     if(opcion==2)
     {
